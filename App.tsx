371859d247b6bc7103ec9ec5a36ad93d792c76fe
Gulab Golani <gulabgolaniwork@gmail.com>
2026-05-17 18:25:24 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <div style={{
+      height: "100vh",
+      background: "#0f172a",
+      color: "white",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+      fontFamily: "Arial"
+    }}>
+      <h1 style={{ fontSize: "60px" }}>
+        My First React Website 🚀
+      </h1>
+
+      <p style={{ fontSize: "20px", color: "#94a3b8" }}>
+        Built with React + Vite
+      </p>
+    </div>
+  )
+}
+
+export default App
